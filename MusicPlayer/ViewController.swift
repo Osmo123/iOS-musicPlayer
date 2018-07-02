fa286a87bc89b3f410bdb426207898e6f0082ed9
aslam <aslam.a@osmosys.asia>
2018-07-02 18:39:32 +0530

    Added viewcontroller file

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Mobile Team on 02/07/18.
+//  Copyright © 2018 Cullen Mooney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
